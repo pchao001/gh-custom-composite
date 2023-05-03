4ab3d1376a0287f7df76402ae083018e8bd4b43c
LAPTOP-SV9R2L8C\pchao <pchao00@yahoo.com>
2023-05-03 22:14:10 +0800

    depploy S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    const bucket = core.getInput('bucket', { required:true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // Upload
+    const s3Uri = `s3://${bucket}`;
+    //AWS_ACCESS_KEY_ID=AKIAT3PD7Y5GZA6DBA6K
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)  
+
     core.notice('Hello from my javascript custom action.')
 }
 run();
